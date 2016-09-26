be8ef0452869fbdb6cb016ff43228a66aa6c8d19
Varsity Software <github@varsitysoftware.com>
2016-09-26 08:58:29 -0400

    Create socialsharingtwitter.js

---
+++ www/socialsharingtwitter.js
@@ -0,0 +1,29 @@
+/*global cordova,window,console*/
+/**
+ * A Social Sharing for Twitter plugin for Cordova
+ * 
+ * Developed by John Weaver for Varsity Software
+ */
+
+var SocialSharingTwitter = function ()
+{
+
+};
+
+SocialSharingTwitter.prototype.delete = function (success, fail, options)
+{
+    if (!options) {
+        options = {};
+    }
+
+    var params = {
+        fileURL: options.fileURL ? options.fileURL : null,
+        fileType: options.fileType ? options.fileType : null,
+        message: options.message ? options.message : null        
+    };
+
+    return cordova.exec(success, fail, "SocialSharingTwitter", "sendTweet", [params]);
+
+};
+
+window.socialSharingTwitter = new SocialSharingTwitter();
